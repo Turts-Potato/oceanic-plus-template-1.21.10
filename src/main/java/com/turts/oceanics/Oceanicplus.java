810e5383ca7e946c578c4859e3de04e5cbc41e04
Turts-Potato <dead.meme075@gmail.com>
2025-11-02 01:35:21 -0800

    dwas

---
+++ src/main/java/com/turts/oceanics/Oceanicplus.java
@@ -0,0 +1,16 @@
+package com.turts.oceanics;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Oceanicplus implements ModInitializer {
+	public static final String MOD_ID = "oceanic-plus";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/turts/oceanics/OceanicplusClient.java
@@ -0,0 +1,10 @@
+package com.turts.oceanics;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OceanicplusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
